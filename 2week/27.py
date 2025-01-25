8f0a4ea07d33a8c811774f6afdd1a7514c8aeb26
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 13:20:51 +0500

    unpack tuples part2

---
+++ 2week/27.py
@@ -0,0 +1,17 @@
+fruits1 = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits1
+
+print(green)
+print(yellow)
+print(red)
+
+print(end="\n")
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
