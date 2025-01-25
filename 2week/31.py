df0dcac9aea616d442ab9992d085469ff4960dd8
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 13:30:28 +0500

    adding sets

---
+++ 2week/31.py
@@ -0,0 +1,12 @@
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+
+print(end="\n")
+
+thisset1 = {"apple", "banana", "cherry"}
+tropical1 = {"pineapple", "mango", "papaya"}
+
+thisset1.update(tropical1)
+
+print(thisset1)
