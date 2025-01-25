326842c50e01dfacb6fa7b1692e35344620926e2
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 10:37:21 +0500

    access items

---
+++ 2week/18.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+print(end="\n")
+
+thislist1 = ["apple", "banana", "cherry"]
+[print(x) for x in thislist1]
