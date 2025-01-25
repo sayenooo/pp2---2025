b163281518c9b9c61313166f2ac3b0e01b715d6e
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 14:27:24 +0500

    key&value

--- 2week/45.py
+++ 2week/45.py
@@ -5,14 +5,17 @@ thisdict =	{
 }
 for x in thisdict:
   print(x) #key
+  print(end=" ")
   print(thisdict[x]) #value
+  print(end="\n")
 
 for x in thisdict.keys():
   print(x) #key
+  print(end="\n")
 for x in thisdict.values():
   print(x) #value
-
+  print(end="\n")
 
 for x, y in thisdict.items():
-  print(x, y) #both
+  print(x," " y) #both
 
