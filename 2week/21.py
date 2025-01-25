ac2dbb898a159f365bd15121b578687c9babe932
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 10:41:21 +0500

    sort descending

---
+++ 2week/21.py
@@ -0,0 +1,9 @@
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+print (end="\n")
+
+thislist1 = [100, 50, 65, 82, 23]
+thislist1.sort(reverse = True)
+print(thislist1)
