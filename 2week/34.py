94e2e696753496c0b7accec53836954be412bb99
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 13:58:22 +0500

    joining sets

---
+++ 2week/34.py
@@ -0,0 +1,18 @@
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+print(end="\n")
+
+set11 = {"a", "b", "c"}
+set22 = {1, 2, 3}
+set33 = set11 | set22
+print(set33)
+
+print(end="\n")
+
+x = {"a", "b", "c"}
+y = (1, 2, 3)
+z = x.union(y)
+print(z)
---
+++ 2week/35.py
@@ -0,0 +1,16 @@
+
+set111 = {"a", "b", "c"}
+set222 = {1, 2, 3}
+set333 = {"John", "Elena"}
+set444 = {"apple", "bananas", "cherry"}
+myset = set111.union(set222, set333, set444)
+print(myset)
+
+print(end="\n")
+
+set01 = {"a", "b", "c"}
+set02 = {1, 2, 3}
+set03 = {"John", "Elena"}
+set04 = {"apple", "bananas", "cherry"}
+myset1 = set01 | set02 | set03 |set04
+print(myset1)
