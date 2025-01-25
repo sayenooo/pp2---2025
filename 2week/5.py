451405ac549d766f7aa722382ade00a0fa2c646e
sayenooo <zhansaya2007ke@gmail.com>
2025-01-25 10:20:42 +0500

    lists operstions

---
+++ 2week/5.py
@@ -0,0 +1,6 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:]) #после 2
+print(thislist[:2]) #2 включительно
+print(thislist[-1])
+print(thislist[0])
+print(thislist[2:4]) #после 2 и 4 включительно
