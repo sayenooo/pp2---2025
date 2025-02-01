f4d06ff15abd067a9882ed9600c49a1dc6f3b93a
sayenooo <zhansaya2007ke@gmail.com>
2025-02-01 15:16:34 +0500

    classes, 1 task

--- 3week/1.py
+++ 3week/1.py
@@ -1,6 +1,12 @@
-def my_function(fname):
-  print(fname + " Refsnes")
-
-my_function("Emil")
-my_function("Tobias")
-my_function("Linus")
+class string:
+    def get_str(self):
+        self.input_str=input()
+    def prt_str(self):
+        print(self.input_str.upper())
+    def rvs_str(self):
+        print(self.input_str[::-1])
+        
+string_manipulation=string()        
+string_manipulation.get_str()
+string_manipulation.prt_str()        
+string_manipulation.rvs_str()
