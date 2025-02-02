47de85d3d0118cecb90b5ac68beda12c58584341
sayenooo <zhansaya2007ke@gmail.com>
2025-02-02 19:28:16 +0500

    is prime?

--- 3week/44.py
+++ 3week/44.py
@@ -1,21 +1,19 @@
-def is_prime(n):
-    if n<=1:
-        return False
-    for i in range(2,n):
-        if n%i==0:
+def filter_prime(a):
+    def is_prime(n):
+        if n <= 1:
             return False
-    return True
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+    return [num for num in a if is_prime(num)]
 
 a = []
 while True:
-    i = int(input())
+    i = int(input("Enter a number (0 to finish): "))
     a.append(i)
-    if i==0:
+    if i == 0:
         break
-    
-b = []
-for i in a:
-    if is_prime(i):
-        b.append(i)
-        
-print(b, end = " ")
+
+prime_numbers = filter_prime(a)
+print(prime_numbers)
