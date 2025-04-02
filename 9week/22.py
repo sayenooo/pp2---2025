cc019b9d5d775dc9cab198243a6774909cb65e09
sayenooo <zhansaya2007ke@gmail.com>
2025-04-02 13:41:40 +0500

    'upd'

--- 9week/22.py
+++ 9week/22.py
@@ -1,5 +1,4 @@
 import pygame  # Импорт библиотеки pygame
-import time  # Импорт библиотеки time
 import random  # Импорт библиотеки random
 
 # Инициализация pygame
@@ -11,8 +10,7 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
-color = (213, 0, 255)
-dgreen = (137,255,252)
+gray = (50, 50, 50)
 
 # Размеры окна
 dis_width = 800  # Ширина
@@ -20,7 +18,7 @@ dis_height = 600  # Высота
 
 # Создание окна игры
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game')  # Заголовок окна
+pygame.display.set_caption('Snake Game')
 
 # Создание часов для управления скоростью змейки
 clock = pygame.time.Clock()
@@ -40,45 +38,31 @@ def generate_walls(level):
         walls.append((wall_x, wall_y))
     return walls
 
-# Функция для отображения счета игрока
 def Your_score(score):
     value = font_style.render("Your Score: " + str(score), True, white)
     dis.blit(value, [0, 0])
 
-# Функция для отображения уровня
 def Your_level(level):
     value = font_style.render("Your Level: " + str(level), True, white)
     dis.blit(value, [600, 0])
 
-# Функция для отрисовки змейки
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, green, [x[0], x[1], snake_block, snake_block])
 
 def gameLoop():
-    game_over = False  # Переменная завершения игры
-    game_close = False  # Переменная проигрыша
-
-    x1 = dis_width / 2
-    y1 = dis_height / 2
-    x1_change = 0
-    y1_change = 0
-
+    game_over = False
+    game_close = False
+    x1, y1 = dis_width / 2, dis_height / 2
+    x1_change, y1_change = 0, 0
     snake_List = []
     Length_of_snake = 1
-
     foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-    
-    foodxx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foodyy = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-    food_time = pygame.time.get_ticks()
-    
-    level = 1
+    level = 0
     walls = generate_walls(level)
 
     while not game_over:
-
         while game_close:
             dis.fill(black)
             message = font_style.render("Game Over! Press Q-Quit or C-Play Again", True, red)
@@ -86,7 +70,6 @@ def gameLoop():
             Your_score(Length_of_snake - 1)
             Your_level(level)
             pygame.display.update()
-
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
@@ -112,56 +95,47 @@ def gameLoop():
                     y1_change = snake_block
                     x1_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0 or (x1, y1) in walls:
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
+        
+        for wall in walls:
+            if (x1, y1) == wall:
+                game_close = True
 
         x1 += x1_change
         y1 += y1_change
         dis.fill(black)
-
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-
-        if pygame.time.get_ticks() - food_time > 5000:
-            food_time = pygame.time.get_ticks()
-            foodxx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foodyy = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-        pygame.draw.rect(dis, dgreen, [foodxx, foodyy, snake_block, snake_block])
-
+        pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
+        
+        for wall in walls:
+            pygame.draw.rect(dis, gray, [wall[0], wall[1], snake_block, snake_block])
+        
         snake_Head = [x1, y1]
         snake_List.append(snake_Head)
-
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
-
+        
         for x in snake_List[:-1]:
             if x == snake_Head:
                 game_close = True
-
+        
         our_snake(snake_block, snake_List)
         Your_score(Length_of_snake - 1)
         Your_level(level)
-
-        for wall in walls:
-            pygame.draw.rect(dis, color, [wall[0], wall[1], snake_block, snake_block])
-
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
             Length_of_snake += 1
-
-        if x1 == foodxx and y1 == foodyy:
-            foodxx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foodyy = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 3
-
-        if Length_of_snake // 5 > level:
-            level += 1
+        
+        new_level = (Length_of_snake - 1) // 2
+        if new_level > level:
+            level = new_level
             walls = generate_walls(level)
-
+        
         clock.tick(snake_speed)
-
+    
     pygame.quit()
     quit()
 
