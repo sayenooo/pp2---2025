bf4a7f4464f3f3be239d81fd9b0eb1b898df2d7d
sayenooo <zhansaya2007ke@gmail.com>
2025-04-18 21:59:47 +0500

    upd

--- 11week/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-def load_config():
-    return {
-        'dbname': 'postgres',
-        'user': 'postgres',
-        'password': 'Loving007',
-        'host': 'localhost',
-        'port': '5433'  # проверь, что у тебя действительно 5433
-    }
