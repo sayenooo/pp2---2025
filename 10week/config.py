d5066aefde01177fba60cb436cb1cf0759d83164
sayenooo <zhansaya2007ke@gmail.com>
2025-04-18 21:57:20 +0500

    upd

--- 10week/config.py
+++ 10week/config.py
@@ -1,16 +1,9 @@
-from configparser import ConfigParser
-def load_config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-    # get section, default to postgresql
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-    return config
-if __name__ == '__main__':
-    config = load_config()
-    print(config)
+# config.py
+def load_config():
+    return {
+        'dbname': 'postgres',
+        'user': 'postgres',
+        'password': 'Loving007',
+        'host': 'localhost',
+        'port': '5433'  # проверь, что у тебя действительно 5433
+    }
